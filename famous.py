a19a228b2c7c31508393c275b7bf99ef15d62997
jherrlin <jherrlin@gmail.com>
2016-02-03 22:06:04 +0100

    Running tests

--- famous.py
+++ famous.py
@@ -9,24 +9,20 @@ people = [
 ]
 
 message = [
-    'Fix this shit up',
     'Add Elvis operator',
     '10 * nested loop',
-    'To drunk....',
     'Writing som heavy code',
     'Commiting like a pro',
-    'Git is the sh*t',
-    'I can kive',
+    'I can jive',
     'Cobol is easy',
     'removing all tests',
     'changed all variables to a,b,c,...',
-    'rm -rf /'
 ]
 
-#random = random.randint(0, len(people)-1)
+ranint = random.randint(0, len(people)-1)
 
-#system('git config --global user.name \"{}\"'.format(people[random][0]))
-#system('git config --global user.email {}'.format(people[random][1]))
+system('git config --global user.name \"{}\"'.format(people[ranint][0]))
+system('git config --global user.email {}'.format(people[ranint][1]))
 
 print('You are not commiting as:')
 call(['git', 'config', 'user.name'])
@@ -34,10 +30,10 @@ call(['git', 'config', 'user.email'])
 
 b = raw_input('Continue :')
 
-random = random.randint(0, len(message)-1)
+ranint = random.randint(0, len(message)-1)
 system('echo \"yeah\" >> README.md')
 system('git add .')
-system('git commit -m \"{}\"'.format(message[random]))
+system('git commit -m \"{}\"'.format(message[ranint]))
 system('git push')
 system('sed -i \'$s/yeah$//\' README.md')
 
